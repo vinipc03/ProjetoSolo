5e9d13ffcda6fdc7094ca8bee720888fa2d8b34a
Vinícius Costa <vini.pc03@gmail.com>
2024-01-31 14:00:06 -0300

    Added Player animations

---
+++ Project Legend of Lina/Assets/Player/Script/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 vel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        vel = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = vel;
+    }
+}
