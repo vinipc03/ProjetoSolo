2aa227970ae7b31b3879b47222ebbb16f27cf491
Vinícius Costa <vini.pc03@gmail.com>
2024-01-31 18:05:33 -0300

    Movement and Jump Added

--- Project Legend of Lina/Assets/Player/Script/PlayerController.cs
+++ Project Legend of Lina/Assets/Player/Script/PlayerController.cs
@@ -7,6 +7,11 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
     Vector2 vel;
 
+    public Transform floorCollider;
+    public LayerMask floorLayer;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +22,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        vel = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
 
+        Jump();
+        Movement();
 
     }
 
@@ -27,4 +32,24 @@ public class PlayerController : MonoBehaviour
     {
         rb.velocity = vel;
     }
+
+
+    #region Movements
+
+    void Movement() //MOVIMENTAÇÃO DIREITA E ESQUERDA 
+    {
+        vel = new Vector2(Input.GetAxisRaw("Horizontal") * 4, rb.velocity.y);
+    }
+
+    void Jump() //PULO 
+    {
+        bool canJump = Physics2D.OverlapCircle(floorCollider.position, 0.3f, floorLayer);
+        if (canJump && Input.GetButtonDown("Jump")) // && comboTime > 0.5f
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(new Vector2(0, 600));
+        }
+    }
+
+    #endregion
 }
