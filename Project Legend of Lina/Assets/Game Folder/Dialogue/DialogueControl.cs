490136ba1b8a2ed3262c87bfb73ff85be437abb8
Vinícius Costa <vini.pc03@gmail.com>
2024-02-13 11:56:23 -0300

    Making a dialogue system

---
+++ Project Legend of Lina/Assets/Game Folder/Dialogue/DialogueControl.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueControl : MonoBehaviour
+{
+
+    [Header("Components")]
+    public GameObject dialogueObj; //JANELA DO DIALOGO
+    public Image profileSprite; // SPRITE DO PERFIL
+    public Text speechText; // TEXTO DA FALA
+    public Text actorNameText; // NOME DO NPC
+
+
+    [Header("Settings")]
+    public float typingSpeed; //VELOCIDADE DA FALA
+
+    //VARIÁVEIS DE CONTROLE
+    private bool isShowing; //SE A JANAELA ESTÁ VISÍVEL
+    private int index; // INDEX DAS SENTENÇAS
+    private string[] sentences;
+
+    public static DialogueControl instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator TypeSentence()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            speechText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+
+        }
+    }
+
+    //PULA PARA PRÓXIMA FALA
+    public void NextSentence()
+    {
+        if(speechText.text == sentences[index])
+        {
+            if(index < sentences.Length - 1)
+            {
+                index++;
+                speechText.text = "";
+                StartCoroutine(TypeSentence());
+            }
+            else //QUANDO TERMINA OS TEXTOS
+            {
+                speechText.text = "";
+                index = 0;
+            }
+        }
+    }
+
+    //CHAMA A FALA
+    public void Speech(string[] txt)
+    {
+        if (!isShowing)
+        {
+            dialogueObj.SetActive(true);
+            sentences = txt;
+            StartCoroutine(TypeSentence());
+            isShowing = true;
+        }
+    }
+}
---
+++ Project Legend of Lina/Assets/Game Folder/Dialogue/NPCDialogue.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDialogue : MonoBehaviour
+{
+    public float dialogueRange;
+    public LayerMask playerLayer;
+    public DialogueSettings dialogue;
+    bool playerHit;
+    private List<string> sentences = new List<string>();
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GetNPCInfo();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerHit)
+        {
+            DialogueControl.instance.Speech(sentences.ToArray());
+        }
+    }
+
+    void GetNPCInfo()
+    {
+        for(int i=0; i < dialogue.dialogues.Count; i++)
+        {
+            sentences.Add(dialogue.dialogues[i].sentence.english);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        ShowDialogue();
+    }
+
+    void ShowDialogue()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, dialogueRange, playerLayer);
+
+        if(hit != null)
+        {
+            playerHit = true;
+            //COLOCAR ANIMAÇÃO DE EMOTE
+        }
+        else
+        {
+            playerHit = false;
+            DialogueControl.instance.dialogueObj.SetActive(false);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, dialogueRange);
+    }
+
+}
